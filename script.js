8df404a510ac5f2a45c2021836f68eb5c561fe04
Rian Seabra <rianlealseabra@gmail.com>
2023-04-26 18:48:05 -0300

    concertando erro

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = () => {
    const erroEmail = () => {
       const email = (document.querySelector("#email").value);
       const senha = (document.querySelector("#password").value);
-      if (email == "trybe@test.com" && senha == "123456") {
+      if (email == "tryber@teste.com" && senha == "123456") {
          alert("Olá, Tryber!")
       } else {
          alert("Email ou senha inválidos.");
