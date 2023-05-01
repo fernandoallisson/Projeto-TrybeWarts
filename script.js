e46ec4cc851b0045cd56a246dbe7a2262ce37e89
Fernando Álisson <fernando.alisson@ufpe.br>
2023-05-01 15:38:47 -0300

    Alterando o index e o script

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
+const button = (document.querySelector('#button')); // Pega  o botão enviar (o botão de submeter do form do header);
+const counter = document.querySelector('#textareacounter'); // A div que conterá a quantidade de caracteres faltando para parar de escrever;
+const area = document.querySelector('#textareainput'); // Essa é a área que terá no máximo 500 caracteres.
+const contador = 500;
+
+const erroEmail = () => {
+  const email = (document.querySelector('#email').value); // Pega o input do e-mail;
+  const senha = (document.querySelector('#password').value); // Pega o input de senha;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+};
+
+const check = () => {
+  const checar = document.querySelector('#agreement'); // Checkbox de verificação para que o #submit-btn seja ativado;
+  if (checar.checked) {
+    return true;
+  } return false;
+};
+
+const verificacaoEnviar = () => {
+  const enviar = document.querySelector('#submit-btn'); // Pega  o botão enviar (o botão de submeter do form da main);
+  if (check()) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+};
+
 window.onload = () => {
+  const checar = document.querySelector('#agreement'); // Checkbox de verificação para que o #submit-btn seja ativado;
+  checar.addEventListener('click', verificacaoEnviar); // Isso vai pegar a função para verificar se o check está habilitado para fazer o envio das informações, e vice-versa;
 
-   const button = (document.querySelector("#button"));
-
-   const erroEmail = () => {
-      const email = (document.querySelector("#email").value);
-      const senha = (document.querySelector("#password").value);
-      if (email == "tryber@teste.com" && senha == "123456") {
-         alert("Olá, Tryber!")
-      } else {
-         alert("Email ou senha inválidos.");
-      };
-   }
-   button.addEventListener("click", erroEmail);
-
-   const enviar = document.querySelector("#submit-btn");
-   const checar = document.querySelector("#agreement");
-
-   const check = () => {
-      if (checar.checked) {
-         return true;
-      } return false;
-
-   }
-
-   const verificacaoEnviar = () => {
-      if (check()) {
-         enviar.disabled = false;
-      } else {
-         enviar.disabled = true;
-      } 
-   }
-
-   checar.addEventListener('click', verificacaoEnviar);
-
-   const counter = document.querySelector("#textareacounter");
-   const area = document.querySelector("#textareainput");
-   let contador = 500
-  
-   area.addEventListener("input",() => {
-   
-const numeroCarac = area.value.length; 
-counter.innerHTML = contador - numeroCarac
-   
-    
-   })
-}
+  button.addEventListener('click', erroEmail);
+  area.addEventListener('input', () => {
+    const numeroCarac = area.value.length;
+    counter.innerHTML = contador - numeroCarac;
+  });
+};
