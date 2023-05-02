737b00c894871255c12ebda406bb6ce423e04574
Fernando Álisson <fernando.alisson@ufpe.br>
2023-05-01 23:08:46 -0300

    Ultimo commit?

--- cypress.config.js
+++ cypress.config.js
@@ -11,11 +11,5 @@ module.exports = defineConfig({
   viewportHeight: 768,
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 10000,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
-  },
+
 });
